911bdb2f0ebd27021732c7643ee511eb2356fcdf
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-08-01 14:35:44 +0900

    (feat) Product api warning clear

--- src/main/java/com/next/app/api/user/entity/Product.java
+++ src/main/java/com/next/app/api/user/entity/Product.java
@@ -1,14 +1,19 @@
 package com.next.app.api.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "product_kimyoungji")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,33 +27,4 @@ public class Product {
     @Column
     private String description;
 
-    // Constructors
-    public Product() {}
-    public Product(String name, BigDecimal price, String description) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public BigDecimal getPrice() {
-        return price;
-    }
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
